a4aec72e675cba506e46e907abf620240238ba30
kouhei-ioroi <kouhei@ioroi.group>
2026-05-17 03:49:46 +0900

    実装

---
+++ Source/Setup-BitLockerPIN.ps1
@@ -0,0 +1,56 @@
+﻿$PresetPin  = "123456"
+
+# BitLocker状態取得
+$blv = Get-BitLockerVolume -MountPoint $env:SystemDrive
+
+# プリセットPINをSecureStringに変換
+$SecurePin = ConvertTo-SecureString $PresetPin -AsPlainText -Force
+
+# --- ケース1: BitLocker未有効 ---
+if ($blv.VolumeStatus -eq "FullyDecrypted") {
+    Write-Output "BitLockerが無効のため、有効化 + TPM+PINを設定します。"
+
+    if (-not ($blv.KeyProtector | Where-Object { $_.KeyProtectorType -eq "RecoveryPassword" })) {
+        Add-BitLockerKeyProtector `
+            -MountPoint $env:SystemDrive `
+            -RecoveryPasswordProtector
+    }
+
+    Enable-BitLocker `
+        -MountPoint $env:SystemDrive `
+        -TpmAndPinProtector `
+        -Pin $SecurePin `
+        -SkipHardwareTest
+
+    # タスクスケジューラにPIN変更タスクを登録
+    & "$PSScriptRoot\Register-Task.ps1"
+
+}elseif ($blv.KeyProtector | Where-Object { $_.KeyProtectorType -eq "Tpm" }) {
+    Write-Output "TPMのみのため、PIN必須に変更します。"
+
+    if (-not ($blv.KeyProtector | Where-Object { $_.KeyProtectorType -eq "RecoveryPassword" })) {
+        Add-BitLockerKeyProtector `
+            -MountPoint $env:SystemDrive `
+            -RecoveryPasswordProtector
+    }
+
+    Add-BitLockerKeyProtector `
+        -MountPoint $env:SystemDrive `
+        -TpmAndPinProtector `
+        -Pin $SecurePin `
+        -SkipHardwareTest
+
+    # タスクスケジューラにPIN変更タスクを登録
+    & "$PSScriptRoot\Register-Task.ps1"
+
+}elseif (-not (Test-Path -Path "C:\Scripts\.PinChanged")) {
+    Write-Output "TPM+PINは設定済みですが、PIN変更が完了していないため、PIN変更タスクを登録します。"
+    # タスクスケジューラにPIN変更タスクを登録
+    & "$PSScriptRoot\Register-Task.ps1"
+
+}else {
+    Write-Output "既にTPM+PINが設定され、変更済みです。変更は行いません。"
+
+}
+
+exit 0
---
+++ Source/Verify-BitLockerPIN.ps1
@@ -0,0 +1,16 @@
+﻿# BitLocker状態取得
+$blv = Get-BitLockerVolume -MountPoint $env:SystemDrive
+
+if ($blv.VolumeStatus -eq "FullyDecrypted") {
+    Write-Output "BitLockerが有効ではありません。"
+    exit 1
+}elseif (-not ($blv.KeyProtector | Where-Object { $_.KeyProtectorType -eq "TpmPin" })) {
+    Write-Output "TPM+PINが設定されていません。"
+    exit 1
+}elseif (-not (Test-Path -Path "C:\Scripts\.PinChanged")) {
+    Write-Output "TPM+PINは設定されていますが、PIN変更が完了していません。"
+    exit 0
+}else {
+    Write-Output "BitLockerが有効で、TPM+PINが既に設定され、変更済みです。"
+    exit 0
+}
